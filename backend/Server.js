807c206da86e244ff3897915591b2e08026e153a
Deeksha Muralikumar <deekshamkt@gmail.com>
2026-03-02 21:27:49 +0530

    ContactManager

--- backend/Server.js
+++ backend/Server.js
@@ -10,7 +10,9 @@ const User = require('./entity/User');
 const Contact = require('./entity/Contact');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://contactmanagerfe.onrender.com'
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
